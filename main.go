f06b00cb7eff4dedd8d89c472b36514b4b280dcf
askvart <tur1amant@gmail.com>
2023-06-23 20:22:26 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net/http"
+
+	//"github.com/gorilla/mux"
+	"askvart.com/goals/views"
+	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	homeView    *views.View
+	contactView *views.View
+	faqView     *views.View
+	aboutView   *views.View
+)
+
+func home(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	must(homeView.Render(w, nil))
+}
+
+func contact(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	must(contactView.Render(w, nil))
+}
+
+func faq(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	must(faqView.Render(w, nil))
+}
+func about(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	w.Header().Set("Content-Type", "text/html")
+	must(aboutView.Render(w, nil))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	//r := mux.NewRouter()
+	//r.NotFoundHandler = http.HandlerFunc(page404)
+	//r.HandleFunc("/", home)
+	//r.HandleFunc("/contact", contact)
+	//r.HandleFunc("/faq", faq)
+	//http.ListenAndServe(":3000", r)
+
+	homeView = views.NewView("bootstrap", "views/home.gohtml")
+	contactView = views.NewView("bootstrap", "views/contact.gohtml")
+	faqView = views.NewView("bulma", "views/faq.gohtml")
+	aboutView = views.NewView("tailwind", "views/about.gohtml")
+
+	router := httprouter.New()
+	router.NotFound = http.FileServer(http.Dir("/static/"))
+	router.GET("/", home)
+	router.GET("/contact", contact)
+	router.GET("/faq", faq)
+	router.GET("/about", about)
+	http.ListenAndServe(":3000", router)
+}
