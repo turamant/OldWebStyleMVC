48042afe8d2725e7839e143459c063b83a9902db
askvart <tur1amant@gmail.com>
2023-06-26 13:30:08 +0300

    connect with UserService

--- main.go
+++ main.go
@@ -1,19 +1,41 @@
 package main
 
 import (
-	"net/http"	
+	"fmt"
+	"net/http"
+
 	"askvart.com/goals/controllers"
+	"askvart.com/goals/models"
 	"github.com/julienschmidt/httprouter"
 	//"github.com/gorilla/mux"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "spartak"
+	password = "spartak"
+	dbname   = "mygolang1"
+)
+
+
+
 func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+				"password=%s dbname=%s sslmode=disable",
+				host, port, user, password, dbname)
 	//r := mux.NewRouter()
 	//r.NotFoundHandler = http.HandlerFunc(page404)
 	//r.HandleFunc("/", home)
 	//r.HandleFunc("/contact", contact)
 	//r.HandleFunc("/faq", faq)
 	//http.ListenAndServe(":3000", r)
+	us, err := models.NewUserService(psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer us.Close()
+	us.AutoMigrate()
 	  
 	usersC := controllers.NewUsers()
 	staticC := controllers.NewStatic()
